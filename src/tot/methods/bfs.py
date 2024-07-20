14eed4e2c9731fd7c7a0dffbc834037c24d5e68c
Jakub 'lambdaofgod' Bartczuk <bartczukkuba@gmail.com>
2024-07-20 15:31:55 +0200

    add support to local ollama lms

--- src/tot/methods/bfs.py
+++ src/tot/methods/bfs.py
@@ -1,17 +1,21 @@
 import itertools
 import numpy as np
 from functools import partial
-from tot.models import gpt
+from tot.models import llm_generate
+from tqdm.auto import tqdm
+from openai import OpenAI
+
 
 def get_value(task, x, y, n_evaluate_sample, cache_value=True):
     value_prompt = task.value_prompt_wrap(x, y)
     if cache_value and value_prompt in task.value_cache:
         return task.value_cache[value_prompt]
-    value_outputs = gpt(value_prompt, n=n_evaluate_sample, stop=None)
+    value_outputs = llm_generate(value_prompt, n=n_evaluate_sample, stop=None)
     value = task.value_outputs_unwrap(x, y, value_outputs)
     if cache_value:
         task.value_cache[value_prompt] = value
     return value
+
 
 def get_values(task, x, ys, n_evaluate_sample, cache_value=True):
     values = []
@@ -19,22 +23,26 @@ def get_values(task, x, ys, n_evaluate_sample, cache_value=True):
     for y in ys:  # each partial output
         if y in local_value_cache:  # avoid duplicate candidates
             value = 0
-        else:    
-            value = get_value(task, x, y, n_evaluate_sample, cache_value=cache_value)
+        else:
+            value = get_value(task, x, y, n_evaluate_sample,
+                              cache_value=cache_value)
             local_value_cache[y] = value
         values.append(value)
     return values
 
+
 def get_votes(task, x, ys, n_evaluate_sample):
     vote_prompt = task.vote_prompt_wrap(x, ys)
-    vote_outputs = gpt(vote_prompt, n=n_evaluate_sample, stop=None)
+    vote_outputs = llm_generate(vote_prompt, n=n_evaluate_sample, stop=None)
     values = task.vote_outputs_unwrap(vote_outputs, len(ys))
     return values
 
-def get_proposals(task, x, y): 
+
+def get_proposals(task, x, y):
     propose_prompt = task.propose_prompt_wrap(x, y)
-    proposals = gpt(propose_prompt, n=1, stop=None)[0].split('\n')
+    proposals = llm_generate(propose_prompt, n=1, stop=None)[0].split('\n')
     return [y + _ + '\n' for _ in proposals]
+
 
 def get_samples(task, x, y, n_generate_sample, prompt_sample, stop):
     if prompt_sample == 'standard':
@@ -43,20 +51,33 @@ def get_samples(task, x, y, n_generate_sample, prompt_sample, stop):
         prompt = task.cot_prompt_wrap(x, y)
     else:
         raise ValueError(f'prompt_sample {prompt_sample} not recognized')
-    samples = gpt(prompt, n=n_generate_sample, stop=stop)
+    samples = llm_generate(prompt, n=n_generate_sample, stop=stop)
     return [y + _ for _ in samples]
 
+
+def get_client(model):
+    if model.startswith("gpt"):
+        client = OpenAI()
+    else:
+        client = OpenAI(
+            base_url="http://localhost:11434/v1", api_key="ollama")
+    return client
+
+
 def solve(args, task, idx, to_print=True):
-    global gpt
-    gpt = partial(gpt, model=args.backend, temperature=args.temperature)
-    print(gpt)
+    global llm_generate
+    client = get_client(args.backend)
+    llm_generate = partial(llm_generate, client=client, model=args.backend,
+                           temperature=args.temperature)
+    print(llm_generate)
     x = task.get_input(idx)  # input
     ys = ['']  # current output candidates
     infos = []
-    for step in range(task.steps):
+    for step in tqdm(range(task.steps)):
         # generation
         if args.method_generate == 'sample':
-            new_ys = [get_samples(task, x, y, args.n_generate_sample, prompt_sample=args.prompt_sample, stop=task.stops[step]) for y in ys]
+            new_ys = [get_samples(task, x, y, args.n_generate_sample,
+                                  prompt_sample=args.prompt_sample, stop=task.stops[step]) for y in ys]
         elif args.method_generate == 'propose':
             new_ys = [get_proposals(task, x, y) for y in ys]
         new_ys = list(itertools.chain(*new_ys))
@@ -70,27 +91,35 @@ def solve(args, task, idx, to_print=True):
         # selection
         if args.method_select == 'sample':
             ps = np.array(values) / sum(values)
-            select_ids = np.random.choice(ids, size=args.n_select_sample, p=ps).tolist()
+            select_ids = np.random.choice(
+                ids, size=args.n_select_sample, p=ps).tolist()
         elif args.method_select == 'greedy':
-            select_ids = sorted(ids, key=lambda x: values[x], reverse=True)[:args.n_select_sample]
+            select_ids = sorted(ids, key=lambda x: values[x], reverse=True)[
+                :args.n_select_sample]
         select_new_ys = [new_ys[select_id] for select_id in select_ids]
 
         # log
-        if to_print: 
-            sorted_new_ys, sorted_values = zip(*sorted(zip(new_ys, values), key=lambda x: x[1], reverse=True))
-            print(f'-- new_ys --: {sorted_new_ys}\n-- sol values --: {sorted_values}\n-- choices --: {select_new_ys}\n')
-        
-        infos.append({'step': step, 'x': x, 'ys': ys, 'new_ys': new_ys, 'values': values, 'select_new_ys': select_new_ys})
+        if to_print:
+            sorted_new_ys, sorted_values = zip(
+                *sorted(zip(new_ys, values), key=lambda x: x[1], reverse=True))
+            print(
+                f'-- new_ys --: {sorted_new_ys}\n-- sol values --: {sorted_values}\n-- choices --: {select_new_ys}\n')
+
+        infos.append({'step': step, 'x': x, 'ys': ys, 'new_ys': new_ys,
+                     'values': values, 'select_new_ys': select_new_ys})
         ys = select_new_ys
-    
-    if to_print: 
+
+    if to_print:
         print(ys)
     return ys, {'steps': infos}
 
+
 def naive_solve(args, task, idx, to_print=True):
-    global gpt
-    gpt = partial(gpt, model=args.backend, temperature=args.temperature)
-    print(gpt)
+    global llm_generate
+    llm_generate = partial(llm_generate, model=args.backend,
+                           temperature=args.temperature)
+    print(llm_generate)
     x = task.get_input(idx)  # input
-    ys = get_samples(task, x, '', args.n_generate_sample, args.prompt_sample, stop=None)
+    ys = get_samples(task, x, '', args.n_generate_sample,
+                     args.prompt_sample, stop=None)
     return ys, {}
--- src/tot/models.py
+++ src/tot/models.py
@@ -1,6 +1,7 @@
 import os
 import openai
-import backoff 
+import backoff
+import ollama
 
 completion_tokens = prompt_tokens = 0
 
@@ -9,33 +10,39 @@ if api_key != "":
     openai.api_key = api_key
 else:
     print("Warning: OPENAI_API_KEY is not set")
-    
+
 api_base = os.getenv("OPENAI_API_BASE", "")
 if api_base != "":
     print("Warning: OPENAI_API_BASE is set to {}".format(api_base))
     openai.api_base = api_base
 
-@backoff.on_exception(backoff.expo, openai.error.OpenAIError)
-def completions_with_backoff(**kwargs):
-    return openai.ChatCompletion.create(**kwargs)
 
-def gpt(prompt, model="gpt-4", temperature=0.7, max_tokens=1000, n=1, stop=None) -> list:
+@backoff.on_exception(backoff.expo, openai.OpenAIError)
+def completions_with_backoff(client, **kwargs):
+    return client.chat.completions.create(**kwargs)
+
+
+def llm_generate(prompt, client=None, model=None, temperature=0.7, max_tokens=1000, n=1, stop=None) -> list:
     messages = [{"role": "user", "content": prompt}]
-    return chatgpt(messages, model=model, temperature=temperature, max_tokens=max_tokens, n=n, stop=stop)
-    
-def chatgpt(messages, model="gpt-4", temperature=0.7, max_tokens=1000, n=1, stop=None) -> list:
+    return chatgpt(messages, model=model, client=client, temperature=temperature, max_tokens=max_tokens, n=n, stop=stop)
+
+
+def chatgpt(messages, client, model, temperature=0.7, max_tokens=1000, n=1, stop=None) -> list:
     global completion_tokens, prompt_tokens
     outputs = []
     while n > 0:
         cnt = min(n, 20)
         n -= cnt
-        res = completions_with_backoff(model=model, messages=messages, temperature=temperature, max_tokens=max_tokens, n=cnt, stop=stop)
-        outputs.extend([choice["message"]["content"] for choice in res["choices"]])
+        res = completions_with_backoff(
+            client, model=model, messages=messages, temperature=temperature, max_tokens=max_tokens, n=cnt, stop=stop)
+        outputs.extend([choice.message.content
+                        for choice in res.choices])
         # log completion tokens
-        completion_tokens += res["usage"]["completion_tokens"]
-        prompt_tokens += res["usage"]["prompt_tokens"]
+        completion_tokens += res.usage.completion_tokens
+        prompt_tokens += res.usage.prompt_tokens
     return outputs
-    
+
+
 def gpt_usage(backend="gpt-4"):
     global completion_tokens, prompt_tokens
     if backend == "gpt-4":
